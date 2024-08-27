21e204681f416c569e33a7e8ac06d96ea9e3a94e
Renzo Pereira <renzogpereira@gmail.com>
2024-08-26 21:38:48 -0300

    Sliders e imagenes

--- src/components/details/details.jsx
+++ src/components/details/details.jsx
@@ -2,6 +2,11 @@ import { doc, getDoc } from "firebase/firestore";
 import { useParams } from "react-router-dom";
 import { db } from "../../firebase/config";
 import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Details = () => {
   const [item, setItem] = useState(null);
@@ -18,7 +23,40 @@ const Details = () => {
     <main>
       {item && (
         <article className="details">
-          <img src={item.img} alt={item.nombre} />
+          <div className="mini">
+            <img src={item.img} alt={item.nombre} />
+            <img src={item.img1} alt={item.nombre} />
+            <img src={item.img2} alt={item.nombre} />
+          </div>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            autoHeight={true}
+            navigation={{
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            }}
+            pagination={{
+              el: ".swiper-pagination",
+              clickable: true,
+            }}
+            modules={[Navigation, Pagination]}
+          >
+            <SwiperSlide>
+              <img src={item.img} alt={item.nombre} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={item.img1} alt={item.nombre} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={item.img2} alt={item.nombre} />
+            </SwiperSlide>
+
+            <div className="swiper-button-next"></div>
+            <div className="swiper-button-prev"></div>
+
+            <div className="swiper-pagination"></div>
+          </Swiper>
           <div>
             <h4 className="marca-details">{item.marca}</h4>
             <h3 className="name-details">{item.nombre}</h3>
